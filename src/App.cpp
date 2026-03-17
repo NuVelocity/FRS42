834d7c939c9462d0c3f1405602ed1b70d019a34c
Francis Dominic Fajardo <fajardo@duck.com>
2026-03-17 14:19:46 +0800

    fix: add missing register calls for object-derived classes.

--- src/App.cpp
+++ src/App.cpp
@@ -7,10 +7,22 @@
 #include <SDL3/SDL_main.h>
 #include <StandAloneFrame.h>
 
+#include "BrickLayout.h"
+#include "CheckPointDefinition.h"
+#include "FloatingBrick.h"
+#include "RoundSet.h"
+
 using namespace nuvelocity;
+using namespace frs42;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
+    // FIXME: This should not be here.
+    ObjectRegistry::Get().Register(BrickLayout::GetClassInfo());
+    ObjectRegistry::Get().Register(CheckPointDefinition::GetClassInfo());
+    ObjectRegistry::Get().Register(FloatingBrick::GetClassInfo());
+    ObjectRegistry::Get().Register(RoundSet::GetClassInfo());
+
 #if DEBUG
     SDL_SetLogPriorities(SDL_LOG_PRIORITY_DEBUG);
 #endif
@@ -26,7 +38,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-    auto* ctx = (Game*)appstate;
+    auto* ctx = static_cast<Game*>(appstate);
 
     if (event->type == SDL_EVENT_QUIT)
     {
@@ -38,7 +50,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    auto* game = (Game*)appstate;
+    auto* game = static_cast<Game*>(appstate);
     game->Update();
     game->Draw();
     return game->mAppResult;
@@ -46,7 +58,7 @@ SDL_AppResult SDL_AppIterate(void* appstate)
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
-    auto* game = (Game*)appstate;
+    auto* game = static_cast<Game*>(appstate);
     delete game;
     SDL_Quit();
 }
